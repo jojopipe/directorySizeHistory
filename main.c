99ea293bd43825805383c7f00016f8c571ebd882
jojopipe <78083515+jojopipe@users.noreply.github.com>
2024-12-02 23:55:21 +0100

    commit message

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <dirent.h>
 #include <regex.h>
-#include <fnmatch.h>
 #include <libgen.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -14,22 +13,27 @@
 static void crawl(char path[], int maxDepth, long *sizeSum);
 
 static const int MAX_DEPTH = 128;
+const char HOME_DIR[64] = "/home/joppo";
 
 void crawl_init(char path[]) {
     long *sizeSum = (long*) malloc(sizeof(long));
     *sizeSum = 0;
     //setup log file
     //put @param path in first line of log file
+
+    char log_dir[1024];
+    snprintf(log_dir, 1024, "%s/%s", HOME_DIR, "dirSizeLogs");
+
     int status;
     mode_t mode = 0000770; //owner and group can do rwx
-    status = mkdir("/home/joppo/dirSizeLogs", mode);
+    status = mkdir(log_dir, mode);
     if (status != 0) {
-        printf("%s\n", "[e] could not create directory. It may already exist.");
+        printf("%s\n", "Could not create directory. It may already exist.");
     }
     char *name = basename(path);
 
-    char *log_file = NULL;
-    snprintf(log_file, 4096, "%s/%s.log", "/home/joppo/dieSizeLogs", name);
+    char log_file[4096];
+    snprintf(log_file, 4096, "%s/%s.log", log_dir, name);
 
     //check if file exists:
     int print_headline = 0;
@@ -39,7 +43,11 @@ void crawl_init(char path[]) {
 
     //make log file for path (if it doesn't exist)
     FILE *file = NULL;
-    file = fopen(log_file, "w");
+    file = fopen(log_file, "a");
+    if (!file) {
+        printf("%s\n", "file could not be opened.");
+        return;
+    }
     //file exists now and is writeable
     if (print_headline)
         fprintf(file, "%s\n", path);
@@ -50,7 +58,7 @@ void crawl_init(char path[]) {
     struct timeval t;
     gettimeofday(&t, NULL);
 
-    fprintf(file, "%lu, %d\n", t.tv_sec, *sizeSum);
+    fprintf(file, "%lu,%ld\n", t.tv_sec, *sizeSum);
 
     fclose(file);
     free(sizeSum);
@@ -67,7 +75,7 @@ static void crawl(char path[], int maxDepth, long *sizeSum) {
     if (!S_ISDIR(st.st_mode)) {
         //path is a not a directory
         long file_size = st.st_size;
-        sizeSum += file_size;
+        *sizeSum += file_size;
         return; //early return
     }
 
@@ -95,7 +103,7 @@ static void crawl(char path[], int maxDepth, long *sizeSum) {
         if(entry->d_type == 8) {
             //file
             lstat(curr_path, &st);
-            sizeSum += st.st_size;
+            *sizeSum += st.st_size;
         }
 
         //invoke recursion:
@@ -109,10 +117,6 @@ int main(int argc, char *argv[]) {
     if (success == -1) {
         return -1;
     }
-
-    // i need:
-    // log file path
-    // some kind of struct, I'm summing up the sizes in
 
     int arguments_count = getNumberOfArguments();
     for (int i = 0; i < arguments_count; ++i) {
